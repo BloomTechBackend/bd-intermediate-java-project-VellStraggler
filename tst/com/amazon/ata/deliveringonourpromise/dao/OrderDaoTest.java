673642611391d817d6461310f4ed0db4611896f7
VellStraggler <david@incwells.com>
2022-12-06 18:25:29 -0700

    puml

--- tst/com/amazon/ata/deliveringonourpromise/dao/OrderDaoTest.java
+++ tst/com/amazon/ata/deliveringonourpromise/dao/OrderDaoTest.java
@@ -1,12 +1,14 @@
 package com.amazon.ata.deliveringonourpromise.dao;
 
+import com.amazon.ata.deliveringonourpromise.App;
 import com.amazon.ata.deliveringonourpromise.types.Order;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class OrderDaoTest {
 
     private OrderDao dao;
-    private final String anOrderId = "900-3746401-0000001";
+    private final String anOrderId = "900-3746401-0000002";
     /** Given
      * An order ID that we know exists
      * When
@@ -15,10 +17,10 @@ public class OrderDaoTest {
      * The result is not null */
     @Test
     public void get_forKnownOrderId_returnsOrder() {
-        Order order = Order.builder()
-                .withOrderId(anOrderId)
-                .build();
-        Order testOrder = dao.get(anOrderId);
-        assert testOrder != null;
+        dao = new OrderDao(App.getOrderManipulationAuthorityClient());
+
+        Order testOrder;
+        testOrder = dao.get(anOrderId);
+        assertNotNull(testOrder);
     }
 }
